56ed649ff17226cac8e52d97b981343e99eb5231
David Sudia <dsudia@gmail.com>
2016-03-01 14:06:36 -0700

    fixed knex logic for rendering pages/sorting

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -9,20 +9,29 @@ function Movies () {
 
 
 router.get('/', function(req, res, next) {
-  var key = Object.keys(req.query);
-  key = key[0];
-  var val = req.query[key];
-  console.log(key, val);
-  knex.select().table('movies').orderBy(key, val)
-    .then(function (data) {
-      console.log(data);
-      return data;
-    }).catch(function (error) {
-      console.log(error);
-    })
-    .then(function (data) {
-      res.render('index', {movies: data});
-    });
+  console.log(req.query);
+  if (Object.keys(req.query).length === 0) {
+    knex.select().table('movies')
+      .then(function(data) {
+        res.render('index', {movies: data});
+      });
+  } else {
+    var key = Object.keys(req.query);
+    key = key[0];
+    var val = req.query[key];
+    console.log(key, val);
+    knex.select().table('movies').orderBy(key, val)
+      .then(function (data) {
+        console.log(data);
+        return data;
+      }).catch(function (error) {
+        console.log(error);
+      })
+      .then(function (data) {
+        res.render('index', {movies: data});
+      });
+  }
+
 });
 
 
