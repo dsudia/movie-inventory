9ad9125a2e07bc8145b27d2e2026027b712c86df
David Sudia <dsudia@gmail.com>
2016-03-01 14:00:58 -0700

    correctly implemented knex file system

--- src/server/knexfile.js
+++ src/server/knexfile.js
@@ -1,13 +1,6 @@
 module.exports = {
-
   development: {
     client: 'pg',
     connection: 'postgres://localhost:5432/movie_inventory_crud'
   },
-
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL
-  }
-
 };
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,31 +2,18 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var escape = require('pg-escape');
-var pgpOptions = {
-  promiseLib: Promise,
-};
-var pgp = require('pg-promise')(pgpOptions);
-require('dotenv').config();
-var cn = 'postgres://localhost:5432/movie_inventory_crud';
-var db = pgp(cn);
+var knex = require('../knex');
+function Movies () {
+  return knex('movies');
+}
 
-// router.get('/', function(req, res, next) {
-//   db.many("select * from movies")
-//     .then(function (data) {
-//       console.log('you rendered the regular index');
-//       return data;
-//     })
-//     .then(function (data) {
-//       res.render('index', {movies: data});
-//     });
-// });
 
 router.get('/', function(req, res, next) {
   var key = Object.keys(req.query);
   key = key[0];
   var val = req.query[key];
   console.log(key, val);
-  db.many("select * from movies order by $1 $2", [key, val])
+  knex.select().table('movies').orderBy(key, val)
     .then(function (data) {
       console.log(data);
       return data;
@@ -38,15 +25,5 @@ router.get('/', function(req, res, next) {
     });
 });
 
-// router.get('/?year=desc', function(req, res, next) {
-//   db.many("select * from movies order by year desc")
-//     .then(function (data) {
-//       console.log(data);
-//       return data;
-//     })
-//     .then(function (data) {
-//       res.render('index', {movies: data});
-//     });
-// });
 
 module.exports = router;
